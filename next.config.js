876f92a6a18781bd07382e75e2a9efba1f006820
FullBarbosa <diibarbosa22@gmail.com>
2022-04-03 16:23:56 -0300

    finish boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with Typescript, React, Next and Styled Components"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyle />
